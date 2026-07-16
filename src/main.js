9065dd1dfc732773b45c721e98168cfaa9d76f54
ch0senn <141234614+ch0senn@users.noreply.github.com>
2026-07-16 16:55:14 +0100

    Initialize Electron app with main window setup

---
+++ src/main.js
@@ -0,0 +1,69 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1400,
+        height: 900,
+        minWidth: 1100,
+        minHeight: 700,
+        backgroundColor: "#202124",
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+            contextIsolation: true,
+            nodeIntegration: false
+        }
+    });
+
+    win.loadFile(path.join(__dirname, "index.html"));
+}
+
+app.whenReady().then(() => {
+
+    createWindow();
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0)
+            createWindow();
+    });
+
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin")
+        app.quit();
+});const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1400,
+        height: 900,
+        minWidth: 1100,
+        minHeight: 700,
+        backgroundColor: "#202124",
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+            contextIsolation: true,
+            nodeIntegration: false
+        }
+    });
+
+    win.loadFile(path.join(__dirname, "index.html"));
+}
+
+app.whenReady().then(() => {
+
+    createWindow();
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0)
+            createWindow();
+    });
+
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin")
+        app.quit();
+});
